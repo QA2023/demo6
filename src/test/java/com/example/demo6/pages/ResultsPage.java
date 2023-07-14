4bc51166239d9853a556ef0a51baf0f67691eead
QA2023 <aleksej-lizunov@mail.ru>
2023-07-14 15:59:03 +0300

    Initial commit

--- src/test/java/com/example/demo6/pages/ResultsPage.java
+++ src/test/java/com/example/demo6/pages/ResultsPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -15,7 +18,12 @@ public class ResultsPage {
     @FindBy(css = ":not(.b_adurl) > cite")
     private List <WebElement> results;
 
+    private WebDriver driver;
     public void clickElement(int num) {
+        WebDriverWait wait = new
+                WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(ExpectedConditions.textToBePresentInElement(results.get(num), "selenium"),
+                ExpectedConditions.elementToBeClickable(results.get(num))));
         results.get(num).click();
         System.out.println("Нажатие на результат под номером" + num);
     }
@@ -28,7 +36,8 @@ public class ResultsPage {
         public  ResultsPage(WebDriver driver){
             PageFactory.initElements(driver,this);
 
-
-
+public ResultsPage(WebDriver driver) {
+                this.driver = driver;
+                PageFactory.initElements(driver, this);
     }
 }
