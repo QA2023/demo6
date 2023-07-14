2aac9d023422d6a6f7148da47ff1b92c9c4893e9
QA2023 <aleksej-lizunov@mail.ru>
2023-07-14 16:20:26 +0300

    Initial commit

--- src/test/java/com/example/demo6/pages/ResultsPage.java
+++ src/test/java/com/example/demo6/pages/ResultsPage.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+
 
 public class ResultsPage {
 
@@ -16,28 +17,28 @@ public class ResultsPage {
     private WebElement searchField;
 
     @FindBy(css = ":not(.b_adurl) > cite")
-    private List <WebElement> results;
+    private List<WebElement> results;
 
     private WebDriver driver;
+
     public void clickElement(int num) {
-        WebDriverWait wait = new
-                WebDriverWait(driver, Duration.ofSeconds(6));
-        wait.until(ExpectedConditions.and(ExpectedConditions.textToBePresentInElement(results.get(num), "selenium"),
-                ExpectedConditions.elementToBeClickable(results.get(num))));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.textToBePresentInElement(results.get(num), "selenium"),
+                ExpectedConditions.elementToBeClickable(results.get(num))
+        ));
         results.get(num).click();
         System.out.println("Нажатие на результат под номером" + num);
     }
 
-    public String getTextFromSearchField(){
-       String val=  searchField.getAttribute("value");
-       System.out.println("В строке поиска текста" +val);
-       return val;
+    public String getTextFromSearchField() {
+        String val = searchField.getAttribute("value");
+        System.out.println("В строке поиска текста" + val);
+        return val;
     }
-        public  ResultsPage(WebDriver driver){
-            PageFactory.initElements(driver,this);
 
-public ResultsPage(WebDriver driver) {
-                this.driver = driver;
-                PageFactory.initElements(driver, this);
+    public ResultsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 }
